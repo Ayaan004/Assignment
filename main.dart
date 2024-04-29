be1c8864f6e342e51b966b9ccfa3517d1a753234
Muhammad Ayaan <162596206+Ayaan004@users.noreply.github.com>
2024-04-29 10:40:29 -0700

    Assigment-7

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+main() {
+//Q2
+// var abc = [0,2,4,6,3,10,11,99,20];
+// abc.sort();
+// print(abc);
+// var largestNumber = abc.last;
+// print(largestNumber);
+
+//Q3
+// var table = 5;;
+// for(var i = 1; i<=10;i++){
+//   print('$table x $i = ${i*table}');
+// }
+
+//Q4
+  // stdout.write("write a word :");
+  // var word = stdin.readLineSync();
+  // var flag = false;
+  // if(word != null){
+  //     var check = word.split('').reversed.join();
+  //     if(check == word){
+  //       flag = true;
+  //     }
+
+  //     if(flag){
+  //       print("It is a Palindrome");
+  //     }
+  //     else{
+  //       print("It is not a Palindrome");
+  //     }
+  // }
+
+  //Q5
+  // var user = 5;
+  // for(var i = 1; i<= user; i++){
+  //    print('${i.toString()*i}');
+  // }
+
+// Q6
+  int i = 1;
+  for (var i = 1; i <= 10; i++) {
+    if (i > 5) {
+      print(i);
+    }
+  }
+}
